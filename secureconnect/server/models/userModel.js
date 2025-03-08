7d74e125442d479235a25cf9e93d6a8b211aaa7d
Thejitha Abeyrathna <ramalthejitha20@gmail.com>
2025-03-08 13:01:16 +0530

    backend usermodel.js

--- secureconnect/server/models/userModel.js
+++ secureconnect/server/models/userModel.js
@@ -1,1 +1,51 @@
 import mongoose from "mongoose";
+
+//Create a schema
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true, //email should be unique
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  verifyOtp: {
+    type: String,
+    default: "",
+  },
+
+  verifyOtpExpiresAt: {
+    type: Number,
+    default: 0,
+  },
+
+  isAccountVerified: {
+    type: Boolean,
+    default: false,
+  },
+
+  resetOtp: {
+    type: String,
+    default: "",
+  },
+
+  resetOtpExpiresAt: {
+    type: Number,
+    default: 0,
+  },
+});
+
+//userModel will only be created if it is not already created
+const userModel = mongoose.models.user || mongoose.model('user', userSchema);
+
+export default userModel;
