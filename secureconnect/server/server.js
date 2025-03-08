a0d7eda8103e06177fc865470be6b0e1d4a22ffb
HarshaYohan <hdankotuwa@gmail.com>
2025-03-08 13:01:47 +0530

    Add server.js and config folder

--- secureconnect/server/server.js
+++ secureconnect/server/server.js
@@ -1,29 +1,29 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import cookieParser from 'cookie-parser';
-import connectDB from './config/mongodb.js';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
 
+import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
-const app = express();
-const port = process.env.port || 4000;
+const app = express(); // initializing express app
+const port = process.env.PORT || 4000;
 connectDB();
 
+const allowedOrigins = ["http://localhost:5173"];
 
-const allowedOrigins = ['http://localhost:5173']
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin:allowedOrigins, credentials: true})) //we can send cookies in the response from the express app
+app.use(cors({ origin: allowedOrigins, credentials: true })); // aloowed to send cookies in the response from the express app
 
-//API Endpoints
-app.get('/',(req,res)=>{
-    res.send("API Working ")
-})
+// API Endpoints
+app.get("/", (req, res) => {
+  res.send("API is Working");
+});
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
-
-
-
-
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
